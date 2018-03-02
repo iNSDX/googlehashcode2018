569ee839797dc6d88c7878d6d5aea8b8469e7d21
eldeantes <angelmarmolfernandez@gmail.com>
2018-03-02 16:17:04 +0100

    Add files via upload

--- hashcode.py
+++ hashcode.py
@@ -32,11 +32,14 @@ class Car:
         return distanceY+distanceX
 
     def choose_ride(self, r1, r2):
-        rideTotal1 = r.distance + self.distance_to_start_ride(r1)
-        rideTotal2 = r.distance + self.distance_to_start_ride(r2)
+        rideTotal1 = r1.distance + self.distance_to_start_ride(r1)
+        rideTotal2 = r2.distance + self.distance_to_start_ride(r2)
 
         if(rideTotal1>rideTotal2): return rideTotal1
         else: return rideTotal2
+
+    def get_total_distance_ride(self, ride):
+        return ride.distance() + self.distance_to_start_ride(ride)
 
     def remove_rides(self, remainingSteps, actualStep):
         ridesCopy = copy.copy(self.rides)
@@ -45,33 +48,61 @@ class Car:
                     or ride.latestTime<=actualStep): self.rides.remove(ride);
 
 
-file  = open("inputs/a_example.in", "r")
+file  = open("inputs/c_no_hurry.in", "r")
 lines=[]
 for line in file:
     lines.append(line)
 
 
 rows, columns, vehiclesNum, ridesNum, bonus, steps = lines[0].split(" ")
+rows = int(rows)
+columns = int(columns)
+vehiclesNum = int(vehiclesNum)
+ridesNum = int(ridesNum)
+bonus = int(steps)
+
 allRides = []
 vehicles = []
 
 for index, ln in enumerate(lines[1:]):
     startX, startY, finishX, finishY, earliestTime, latestTime = ln.split(" ")
+    startX = int(startX)
+    startY = int(startY)
+    finishX = int(finishX)
+    finishY = int(finishY)
+    earliestTime = int(earliestTime)
+    latestTime = int(latestTime)
+    
     ride = Ride(index, startX, startY, finishX, finishY, earliestTime, latestTime)
     allRides.append(ride)
 
-for i in range(0, vehiclesNum):
+for i in range(0, int(vehiclesNum)):
     car = Car(i)
     vehicles.append(car)
 
 
 '''STEPS'''
-for s in range(steps):
-    if not rides:
+for s in range(int(steps)):
+    if not allRides:
         break
-    remaining_steps = steps-s
+    remaining_steps = int(steps)-s
 
     for car in vehicles:
         car.rides = copy.copy(allRides)
         car.remove_rides(remaining_steps, s)
-        '''sort rides'''
+        sorted(car.rides, key=lambda ride: car.get_total_distance_ride(ride))
+        if(car.counter == 0 and car.rides):
+            chosed = car.rides[0]
+            car.chosedRides.append(chosed)
+            allRides.remove(chosed)
+            car.x = chosed.finishX
+            car.y = chosed.finishY
+            car.counter = car.distance_to_start_ride(chosed)+ chosed.distance()
+        car.counter = car.counter -1
+
+writter  = open("inputs/c_no_hurry_output.in", "w")
+for car in vehicles:
+    ln = repr(len(car.chosedRides))
+    for ride in car.chosedRides:
+        ln = ln +" " + repr(ride.id)
+    writter.write(ln + "\n")
